f0423f02170c5de76594fa6debbdefdbf6b3b839
sy <sy@sydeMacBook-Pro.local>
2019-01-20 22:41:53 +0800

    add variables and constant demo.

---
+++ 01_varAndConst/variableAndConstDemo.playground/Contents.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+//comment syntax
+// this is one line comment
+/*
+ these are multiple lines comments
+ comment line 1
+ comment line 2
+ ..,
+ */
+
+//variable and contant
+var myInt_1:Int = 1
+var myInt_2 = 100
+var myInt_3 = myInt_1 + myInt_2
+
+var myFloat_1:Float = 1.123456789987654321
+var myDouble_1:Double = 1.123456789987654321
+
+
+var myStr_1:String = "hello"
+var myStr_2 = "switf"
+var myStr_3 = myStr_1 + " " + myStr_2
+
+let PI:Float = 3.14
+//error, can't assign to a constant
+//PI = 3.14159
+
+
+
+
+
