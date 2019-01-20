f2df62bc991b3c3f90fe97ee8f0bdd337320845e
sy <sy@sydeMacBook-Pro.local>
2019-01-20 23:16:16 +0800

    add tuple demo.

---
+++ 02_tuple/tupleDemo.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+var pos_2d:(_x:Float, _y:Float) = (0,0)
+pos_2d._x = 10.65
+pos_2d._y = -6.4
+pos_2d
+
+pos_2d.0 = -pos_2d._x
+pos_2d.1 = -pos_2d._y
+pos_2d
+
+
+
+var student:(_name:String, _id:Int, _height:Float, _weight:Float) = ("", 0, 0, 0)
+student
+student._name = "yanshuyu"
+student._id = 120370127
+student._height = 170
+student._weight = 54.5
+student
+
+var (name, id, height, _) = student
+name
+id
+height
+
+
+
