b61ea3b21e831c7f9111edb2432517cda36dace4
yanshuyu <zhishuyan@gmail.com>
2019-02-18 16:02:33 +0800

    add optional variable demo.

---
+++ 08_optionalVariable/optionalVariableDemo.playground/Contents.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+var x: Int = 0
+//x = nil -error
+x = 10
+print(x)
+
+
+var y: Int? = nil
+y = 10
+print(y)
+
+//unwrap optional vaeiable
+print(y!)
+
+var name: String? = nil
+name = "yanshuyu"
+print(name)
+print(name!)
+
+
+
