0e2ce8f66980a8f41731722ee0986bd18669822f
yanshuyu <zhishuyan@gmail.com>
2019-02-18 16:24:08 +0800

    add unwrap optional variable related code.

--- 08_optionalVariable/optionalVariableDemo.playground/Contents.swift
+++ 08_optionalVariable/optionalVariableDemo.playground/Contents.swift
@@ -18,5 +18,30 @@ name = "yanshuyu"
 print(name)
 print(name!)
 
+var age: Int? = nil
 
 
+//if let unwrap optional variable
+if let unwrapName = name {
+    print("unwrap name: \(unwrapName)")
+}
+else{
+    print("name is nil")
+}
+
+if let unwrapAge = age {
+    print("unwrap age: \(unwrapAge)")
+}
+else{
+    print("age is nil")
+}
+
+age = 27
+//unwrap multiple optional variable
+if let unwrapName = name,
+    let  unwrapAge = age {
+    print("unwrap name: \(unwrapName)\nunwrap age: \(unwrapAge)")
+}
+else {
+    print("one of name or age is nil")
+}
